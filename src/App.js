32b0b4aaca27640722075ee4cb1309dd37f227e5
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-11-20 20:29:23 +0500

    Make basic structure of the page
    1:- Added adidas.png to store image
    2:- Updated App.js to construct basic structure of the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+// Imports
+// React Imports
+import React from 'react';
+// Images Imports
+import adidas from './adidas.png';
+// Styles Imports
 import './App.css';
 
+// App Function
 function App() {
+
+  // What to Display on the page
   return (
+    // Div for enclosing the complete page
     <div>
-      Project 05
+      {/* Div for card (actual content of the page) */}
+      <div>
+        {/* Div For Displaying Sneeker */}
+        <div>
+          {/* A circle in the background of the sneaker */}
+          <div></div>
+          {/* Sneakers image */}
+          <img src={adidas} alt="adidas"/>
+        </div>
+        {/* div for info of the sneakers */}
+        <div>
+          {/* Title or name of the item */}
+          <h1>Adidas ZX</h1>
+          {/* Item Description */}
+          <h3>FUTURE-READY TRAINERS WITH WRAPPED BOOST FOR EXCEPTION COMFORT.</h3>
+          {/* Div for displaying different buttons showing different sizes */}
+          <div>
+                <button>39</button>
+                <button>40</button>
+                <button>42</button>
+                <button>44</button>
+          </div>
+          {/* Div for purchase button */}
+          <div>
+                <button>Purchase</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
